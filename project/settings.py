e3a51a16aa7974926a3830847eaae7b3ad2e4576
Vladislav Shamrin <shamrin2007@mail.ru>
2022-02-28 19:59:42 +0300

    Added basic settings for the project, created the authorization application.

--- project/settings.py
+++ project/settings.py
@@ -10,11 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import sys
 from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_PATH = BASE_DIR.joinpath('templates')
+PUBLIC_ROOT = BASE_DIR.joinpath('media')
+
+# Insert apps and libs dirs to sys.path
+for path in ('apps', 'libs'):
+    path = str(BASE_DIR.joinpath(path))
+    path in sys.path or sys.path.insert(0, path)
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +39,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +47,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+EXTERNAL_APPS = [
+
+]
+
+PROJECT_APPS = [
+    'authorization',
+]
+
+INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +73,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,12 +135,19 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --------------------------------------------------------------------------------------------------
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = PUBLIC_ROOT.joinpath('static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = PUBLIC_ROOT.joinpath('media')
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = []
